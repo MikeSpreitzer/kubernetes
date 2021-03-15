e2bb0e988fb3a654a2b7e91e18f9f13b7883ca70
Mike Spreitzer <mspreitz@us.ibm.com>
2021-03-15 20:09:52 +0000

    Update generated stuff

--- staging/src/k8s.io/api/flowcontrol/v1beta1/generated.pb.go
+++ staging/src/k8s.io/api/flowcontrol/v1beta1/generated.pb.go
@@ -2651,10 +2651,7 @@ func (m *ConcurrencyLimitStatus) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthGenerated
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthGenerated
 			}
 			if (iNdEx + skippy) > l {
